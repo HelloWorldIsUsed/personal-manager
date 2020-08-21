f714b00cafee3dc62f1eb37839b6baf0aa720faa
xw <xiaowen@cmiot.chinamobile.com>
2020-08-21 16:51:59 +0800

    feature(#game page get data)

--- miniprogram/pages/gameDetail/gameDetail.js
+++ miniprogram/pages/gameDetail/gameDetail.js
@@ -1,66 +1,86 @@
-// miniprogram/pages/gameDetail/gameDetail.js
+// miniprogram/pnums/gameDetail/gameDetail.js
 Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        themeArr: {
+            name: "姓名",
+            num: "号码",
+            score: "得分",
+            rebound: "篮板",
+            assist: "助攻",
+            mistakes: "失误"
+        },
+        itemArr: [
+            {
+                name: "Anna",
+                num: 25,
+                score: 121243423424,
+                rebound: "tianfuStreet",
+                assist: 1,
+                mistakes: 1
+            },
+            {
+                name: "Tina",
+                num: 25,
+                score: 121243423424,
+                rebound: "tianfuStreet",
+                assist: 1,
+                mistakes: 1
+            },
+            {
+                name: "Tom",
+                num: 25,
+                score: 121243423424,
+                rebound: "tianfuStreet",
+                assist: 1,
+                mistakes: 1
+            },
+            {
+                name: "Alex",
+                num: 25,
+                score: 121243423424,
+                rebound: "tianfuStreet",
+                assist: 1,
+                mistakes: 1
+            }
+        ],
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {},
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {},
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {},
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {},
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
