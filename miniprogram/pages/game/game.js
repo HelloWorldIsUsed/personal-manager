a795ff1f351638c1d5ab48cfbf6537b0b5467856
xw <xiaowen@cmiot.chinamobile.com>
2020-09-18 17:24:27 +0800

    feature(#添加比赛记录)

--- miniprogram/pages/game/game.js
+++ miniprogram/pages/game/game.js
@@ -1,11 +1,12 @@
 // miniprogram/pages/game/game.js
-var time = require('./../../utils/formatDate')
+var time = require("./../../utils/formatDate");
 Page({
     /**
      * 页面的初始数据
      */
     data: {
         gameList: [],
+        isManage: false
     },
 
     /**
@@ -13,19 +14,36 @@ Page({
      */
     onLoad: function (options) {
         wx.showToast({
-            title: '加载中',
-            icon: 'loading',
-            mask: true
-        })
+            title: "加载中",
+            icon: "loading",
+            mask: true,
+        });
+        wx.getSetting({
+            success: (res) => {
+                if (res.authSetting["scope.userInfo"]) {
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: (res) => {
+                            if(res.signature == "340f3235fea08b3aba8daa32b104989443ca7bd7" && res.userInfo.nickName == "卖画的小报家"){
+                                this.setData({
+                                    isManage: true
+                                })
+                            }
+                        },
+                    });
+                }
+            },
+        });
+
         var _this = this;
         wx.cloud.callFunction({
             // 云函数名称
             name: "queryGameList",
             success(res) {
                 console.log(res);
-                res.result.map(item => {
-                    item.time = time.formatTimeTwo(new Date(item.time), 'Y-M-D h:m:s')
-                })
+                res.result.map((item) => {
+                    item.time = time.formatTimeTwo(new Date(item.time), "Y-M-D h:m:s");
+                });
                 _this.setData({
                     gameList: res.result,
                 });
@@ -35,38 +53,44 @@ Page({
         });
     },
 
+    onAdd: function () {
+        wx.navigateTo({
+          url: '../add/add',
+        })
+    },
+
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
-    onReady: function () {},
+    onReady: function () { },
 
     /**
      * 生命周期函数--监听页面显示
      */
-    onShow: function () {},
+    onShow: function () { },
 
     /**
      * 生命周期函数--监听页面隐藏
      */
-    onHide: function () {},
+    onHide: function () { },
 
     /**
      * 生命周期函数--监听页面卸载
      */
-    onUnload: function () {},
+    onUnload: function () { },
 
     /**
      * 页面相关事件处理函数--监听用户下拉动作
      */
-    onPullDownRefresh: function () {},
+    onPullDownRefresh: function () { },
 
     /**
      * 页面上拉触底事件的处理函数
      */
-    onReachBottom: function () {},
+    onReachBottom: function () { },
 
     /**
      * 用户点击右上角分享
      */
-    onShareAppMessage: function () {},
+    onShareAppMessage: function () { },
 });
